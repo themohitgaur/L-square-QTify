d5c1aec44a3de066049a60dd3f7cc1abf7dc4e4b
Mohit Gaur <43000112+themohitgaur@users.noreply.github.com>
2024-09-02 08:36:49 +0530

    error solved

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/Search icon.svg";
-// import useAutocomplete from "@mui/base/useAutocomplete";
 import Autocomplete from '@mui/material/Autocomplete';
-
+import TextField from '@mui/material/TextField';
 import { styled } from "@mui/system";
 import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
@@ -21,9 +20,6 @@ const Listbox = styled("ul")(({ theme }) => ({
   maxHeight: "500px",
   zIndex: 10,
   overflowY: "scroll",
-  left: 0,
-  bottom: 0,
-  right: 0,
   listStyle: "none",
   backgroundColor: "var(--color-black)",
   overflow: "auto",
@@ -39,77 +35,60 @@ const Listbox = styled("ul")(({ theme }) => ({
 }));
 
 function Search({ searchData, placeholder }) {
-  const {
-    getRootProps,
-    getInputLabelProps,
-    value,
-    getInputProps,
-    getListboxProps,
-    getOptionProps,
-    groupedOptions,
-  } = Autocomplete({
-    id: "use-autocomplete-demo",
-    options: searchData || [],
-    getOptionLabel: (option) => option.title,
-  });
-
   const navigate = useNavigate();
+
   const onSubmit = (e, value) => {
     e.preventDefault();
-    console.log(value);
-    navigate(`/album/${value.slug}`);
-    //Process form data, call API, set state etc.
+    if (value) {
+      navigate(`/album/${value.slug}`);
+    }
   };
 
   return (
     <div style={{ position: "relative" }}>
-      <form
-        className={styles.wrapper}
-        onSubmit={(e) => {
-          onSubmit(e, value);
+      <Autocomplete
+        freeSolo
+        options={searchData || []}
+        getOptionLabel={(option) => option.title}
+        renderInput={(params) => (
+          <form
+            className={styles.wrapper}
+            onSubmit={(e) => onSubmit(e, params.inputProps.value)}
+          >
+            <TextField
+              {...params}
+              placeholder={placeholder}
+              InputProps={{
+                ...params.InputProps,
+                className: styles.search,
+                endAdornment: (
+                  <button className={styles.searchButton} type="submit">
+                    <SearchIcon />
+                  </button>
+                ),
+              }}
+            />
+          </form>
+        )}
+        renderOption={(props, option) => {
+          const artists = option.songs.reduce((accumulator, currentValue) => {
+            accumulator.push(...currentValue.artists);
+            return accumulator;
+          }, []);
+
+          return (
+            <li {...props} className={styles.listElement}>
+              <div>
+                <p className={styles.albumTitle}>{option.title}</p>
+                <p className={styles.albumArtists}>
+                  {truncate(artists.join(", "), 40)}
+                </p>
+              </div>
+            </li>
+          );
         }}
-      >
-        <div {...getRootProps()}>
-          <input
-            name="album"
-            className={styles.search}
-            placeholder={placeholder}
-            required
-            {...getInputProps()}
-          />
-        </div>
-        <div>
-          <button className={styles.searchButton} type="submit">
-            <SearchIcon />
-          </button>
-        </div>
-      </form>
-      {groupedOptions.length > 0 ? (
-        <Listbox {...getListboxProps()}>
-          {groupedOptions.map((option, index) => {
-            // console.log(option);
-            const artists = option.songs.reduce((accumulator, currentValue) => {
-              accumulator.push(...currentValue.artists);
-              return accumulator;
-            }, []);
-
-            return (
-              <li
-                className={styles.listElement}
-                {...getOptionProps({ option, index })}
-              >
-                <div>
-                  <p className={styles.albumTitle}>{option.title}</p>
-
-                  <p className={styles.albumArtists}>
-                    {truncate(artists.join(", "), 40)}
-                  </p>
-                </div>
-              </li>
-            );
-          })}
-        </Listbox>
-      ) : null}
+        ListboxComponent={Listbox}
+      />
     </div>
   );
 }
